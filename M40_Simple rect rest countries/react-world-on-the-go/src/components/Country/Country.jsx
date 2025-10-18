5b459156f70630adf2de82d5b6ab2b679948ba2a
armmoon4 <rahman15-4343@diu.edu.bd>
2025-10-18 23:05:30 +0600

    use style in react grid 3 coulumn layout 40-4 done

--- M40_Simple rect rest countries/react-world-on-the-go/src/components/Country/Country.jsx
+++ M40_Simple rect rest countries/react-world-on-the-go/src/components/Country/Country.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import './Country.css'
 
 const Country = ({country}) => {
-    console.log(country)
+    const handleVisited = () =>{
+        console.log('visited clicked');
+    }
     return (
-        <div>
+        <div className='country'>
             <h3>Name: {country.name.common}</h3>
             <img src={country.flags.png} alt="" />
             {/* <p>Independent: {country.independent ? 'FREE':'NOT FREE'}</p> */}
             <p>Population: {country.population}</p>
+            <button onClick={handleVisited}>Not Visited</button>
         </div>
     );
 };
