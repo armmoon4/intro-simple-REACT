f278ba268d9410cc8ae4708238f124c183e0f6a2
armmoon4 <rahman15-4343@diu.edu.bd>
2025-10-21 01:03:21 +0600

    42-6 touch done

--- M42_React SPA with scribbles cafe/my-blogs/src/App.jsx
+++ M42_React SPA with scribbles cafe/my-blogs/src/App.jsx
@@ -1,8 +1,16 @@
+import { useState } from 'react'
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
+const [bookmakred , setBookmarked] = useState([]);
+
+  const handleBookMark = (blog)=> {
+      setBookmarked([...bookmakred , blog])
+  }
+
+  console.log(bookmakred)
 
   return (
     <>
@@ -11,11 +19,16 @@ function App() {
 
       <div className="main-container flex text-center">
         <div className="left-container w-[70%]">
-          <Blogs></Blogs>
+
+          <Blogs handleBookMark={handleBookMark}></Blogs>
         </div>
 
         <div className="right-container w-[30%]">
-        selected bloggsss
+        <h1>Reading Time : 0</h1>
+        <h1>Bookmarked Count : 0</h1>
+        {
+          bookmakred.map((marked)=> <p>{marked.title}</p>)
+        }
         </div>
       </div>
     </>
--- M42_React SPA with scribbles cafe/my-blogs/src/components/Blogs/Blogs.jsx
+++ M42_React SPA with scribbles cafe/my-blogs/src/components/Blogs/Blogs.jsx
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import Blog from '../Blog/Blog';
 
 
-const Blogs = () => {
+const Blogs = ({handleBookMark}) => {
 
     const [blogs, setBlogs] = useState([])
 
     useEffect(() => {
         fetch('blogs.json')
-            .then((res) => res.json())
-            .then((data) => setBlogs(data))
+            .then(res => res.json())
+            .then(data => setBlogs(data))
     }, [])
-    console.log(blogs);
+    // console.log(blogs);
     return (
         <div>
             <h1 className='text-3xl'>total : {blogs.length}</h1>
+            <div className="all-blogs grid grid-cols-2">
+                {
+                    blogs.map((blog)=> <Blog key={blog.id} blog={blog} handleBookMark={handleBookMark}></Blog>)
+                }
+            </div>
         </div>
     );
 };
