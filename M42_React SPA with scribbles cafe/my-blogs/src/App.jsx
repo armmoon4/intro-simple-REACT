629d16ccea5b853b77e2cad6d047e38630a47389
armmoon4 <rahman15-4343@diu.edu.bd>
2025-10-20 22:39:54 +0600

    m42-3 done state

--- M42_React SPA with scribbles cafe/my-blogs/src/App.jsx
+++ M42_React SPA with scribbles cafe/my-blogs/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
@@ -6,6 +7,17 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
+      
+
+      <div className="main-container flex text-center">
+        <div className="left-container w-[70%]">
+          <Blogs></Blogs>
+        </div>
+
+        <div className="right-container w-[30%]">
+        selected bloggsss
+        </div>
+      </div>
     </>
   )
 }
