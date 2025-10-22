c0385574bea1491d15a14158cf021bcccd58e02e
armmoon4 <rahman15-4343@diu.edu.bd>
2025-10-22 21:59:08 +0600

    responisve done final touch redy to deploy

--- M42_React SPA with scribbles cafe/assignment-7/src/components/Navbar/Navbar.jsx
+++ M42_React SPA with scribbles cafe/assignment-7/src/components/Navbar/Navbar.jsx
@@ -3,13 +3,39 @@ import { IoMdNotificationsOutline } from "react-icons/io";
 const Navbar = () => {
     return (
         <div>
-            <div className="navbar bg-base-100 shadow-sm px-20">
+            <div className="navbar bg-base-100 shadow-sm px-4 lg:px-20">
                 <div className="navbar-start">
-                    <a className="btn btn-ghost text-xl"><h2 className='text-2xl text-blue-500 flex'>Auction<span className='text-amber-400 '>Gallery</span></h2></a>
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            <li><a>Home</a></li>
+                            <li><a>Auctions</a></li>
+                            <li><a>Categories</a></li>
+                            <li><a>How it works</a></li>
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost text-xl">
+                        <h2 className='text-2xl text-blue-500 flex'>Auction<span className='text-amber-400 '>Gallery</span></h2>
+                    </a>
                 </div>
 
-                <div className='navbar-center'>
-                    <ul className="flex  space-x-6">
+                <div className='navbar-center hidden lg:flex'>
+                    <ul className="flex space-x-6">
                         <li><a href="#">Home</a></li>
                         <li><a href="#">Auctions</a></li>
                         <li><a href="#">Categories</a></li>
@@ -21,13 +47,13 @@ const Navbar = () => {
                     <div className="dropdown dropdown-end">
                         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
                             <div className="indicator">
-                                <IoMdNotificationsOutline size={26} />
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
                                 <span className="badge badge-sm indicator-item">8</span>
                             </div>
                         </div>
                         <div
                             tabIndex={0}
-                            className="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow">
+                            className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow">
                         </div>
                     </div>
                     <div className="dropdown dropdown-end">
@@ -39,8 +65,8 @@ const Navbar = () => {
                             </div>
                         </div>
                         <ul
-                            tabIndex="-1"
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
                             <li>
                                 <a className="justify-between">
                                     Profile
